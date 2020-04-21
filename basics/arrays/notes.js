2539e14645d52b54e73b771447f44826519d902c
Hanif Warren <Hanif.warren@gmail.com>
2020-04-20 23:44:56 -0400

    Section6 lesson39 Manipulating arrays and methods

--- basics/arrays/notes.js
+++ basics/arrays/notes.js
@@ -1,3 +1,9 @@
 //notes.js
 const notes = ['Note 1','Note 2', 'Note 3']
+console.log(notes.pop()); //remove the last entry in the array
+notes.push('My new note') //add to end of array
+notes.shift() // remove first entry in array
+notes.unshift('My first note') // adds entry to beginning of the array
+notes.splice(1,1,'this is the new second item')
+console.log(notes.length)
 console.log(notes)
--- basics/arrays/todo.js
+++ basics/arrays/todo.js
@@ -1,9 +1,14 @@
-//todo.js
+//todo.js lesson 39
 //Challenge create an array with 5 different todos
-//print a message that includes the length
-//print the first and second to last items
-
+//delete the 3rd item
+//add a new item onto the end
+//Remove the first item from the list
 let todo = ['Wake up in the morning',' Excercise for 10 minutes', 'eat a healthy breakfast', 'shower and brush teeth', 'get dressed for success']
-console.log(`You have ${todo.length} things on your menu today`)
-console.log(`The first thing to do is\: ${todo[0]}`)
-console.log(`The second to last thing to do is \: ${todo[todo.length-2]}`)
+console.log(`You have ${todo.length} things on your menu today. Here\'s the original list`)
+console.log(todo)
+
+todo.splice(2,1)
+todo.push('Work as hard as you can')
+todo.shift()
+console.log('Here is your altered list')
+console.log(todo)
