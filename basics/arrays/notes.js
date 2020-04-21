e0075e15f57b646fc21722a7fb7185648701a212
Hanif Warren <Hanif.warren@gmail.com>
2020-04-20 23:30:18 -0400

    Section 6, sections 37 and 38

---
+++ basics/arrays/notes.js
@@ -0,0 +1,3 @@
+//notes.js
+const notes = ['Note 1','Note 2', 'Note 3']
+console.log(notes)
---
+++ basics/arrays/todo.js
@@ -0,0 +1,9 @@
+//todo.js
+//Challenge create an array with 5 different todos
+//print a message that includes the length
+//print the first and second to last items
+
+let todo = ['Wake up in the morning',' Excercise for 10 minutes', 'eat a healthy breakfast', 'shower and brush teeth', 'get dressed for success']
+console.log(`You have ${todo.length} things on your menu today`)
+console.log(`The first thing to do is\: ${todo[0]}`)
+console.log(`The second to last thing to do is \: ${todo[todo.length-2]}`)
