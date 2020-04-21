2fdedc9b9d5b8c6e966c2372c22de1055bfc7f3b
Hanif Warren <Hanif.warren@gmail.com>
2020-04-21 19:10:48 -0400

    41 For loop

--- basics/arrays/notes.js
+++ basics/arrays/notes.js
@@ -8,9 +8,15 @@ notes.splice(1,1,'this is the new second item')
 console.log(notes.length)
 console.log(notes)
 
-console.log(`You have ${notes.length} todos!`)
+console.log(`You have ${notes.length} todos! Here they are in a foreach loop`)
 //forEach is a callback function, it takes the item and the index
 notes.forEach(function (item, index)
 {
     console.log(`Position ${index + 1}. is ${item}`)
 });
+console.log(`You have ${notes.length} todos! Now Here they are in a for loop`);
+for (let count = 0; count <notes.length; count++)
+{
+    const num = count + 1;
+    console.log(`${num}. ${notes[count]}`)
+}
