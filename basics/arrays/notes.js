a04cd368cd51a6e2ce14f1386427ff33f38cc649
Hanif Warren <Hanif.warren@gmail.com>
2020-04-22 23:08:33 -0400

    43 searching arrays challenge

--- basics/arrays/notes.js
+++ basics/arrays/notes.js
@@ -1,22 +1,50 @@
 //notes.js
-const notes = ['Note 1','Note 2', 'Note 3']
-console.log(notes.pop()); //remove the last entry in the array
-notes.push('My new note') //add to end of array
-notes.shift() // remove first entry in array
-notes.unshift('My first note') // adds entry to beginning of the array
-notes.splice(1,1,'this is the new second item')
+const notes = [
+    {
+        title: 'My next trip',
+        body: 'I would like to go to spain'
+    },
+    {
+        title: 'Habits to work on'
+        , body: 'Exercise. Eating a bit better'
+    },
+    {
+        title: 'Office modification',
+        body: 'Get a new seat'
+    }]
+// console.log(notes.pop()); //remove the last entry in the array
+// notes.push('My new note') //add to end of array
+// notes.shift() // remove first entry in array
+// notes.unshift('My first note') // adds entry to beginning of the array
+// notes.splice(1,1,'this is the new second item')
 console.log(notes.length)
 console.log(notes)
 
 console.log(`You have ${notes.length} todos! Here they are in a foreach loop`)
 //forEach is a callback function, it takes the item and the index
-notes.forEach(function (item, index)
-{
+notes.forEach(function (item, index) {
     console.log(`Position ${index + 1}. is ${item}`)
 });
+
 console.log(`You have ${notes.length} todos! Now Here they are in a for loop`);
-for (let count = 0; count <notes.length; count++)
-{
+for (let count = 0; count < notes.length; count++) {
     const num = count + 1;
     console.log(`${num}. ${notes[count]}`)
+}
+const findNote = function (notes, noteTitle) {
+    //findIndex returns the index of the item searched for
+    const index = notes.findIndex(function (note, index) {
+        return note.title === noteTitle
+    })
+    return notes[index]
+}
+const index = notes.findIndex(function (note, index) {
+    return note.title === 'Habits to work on'
+    console.log()
+});
+const find2 = function (notes, noteTitle) {
+    //find goes thru array and returns matching object
+    const index = notes.find(function (note, index) {
+        return note.title === noteTitle
+    })
 }
--- basics/arrays/todo.js
+++ basics/arrays/todo.js
@@ -3,12 +3,40 @@
 //delete the 3rd item
 //add a new item onto the end
 //Remove the first item from the list
-let todo = ['Wake up in the morning',' Excercise for 10 minutes', 'eat a healthy breakfast', 'shower and brush teeth', 'get dressed for success']
-console.log(`You have ${todo.length} things on your menu today. Here\'s the original list`)
-console.log(todo)
+let todos = [{ title: 'Wake up in the morning', isDone : false }
+    , { title: ' Exercise for 10 minutes', isDone : true }
+    , { title: 'eat a healthy breakfast', isDone : true }
+    , { title: 'shower and brush teeth', isDone : true }
+    , { title: 'buy food', isDone : true }
+    , { title: 'get dressed for success', isDone : true }
+]
+console.log(`You have ${todos.length} things on your menu today. Here\'s the original list`)
+console.log(todos)
 
-todo.splice(2,1)
-todo.push('Work as hard as you can')
-todo.shift()
+// todos.splice(2, 1)
+// todos.push('Work as hard as you can')
+// todos.shift()
 console.log('Here is your altered list')
-console.log(todo)
+console.log(todos)
+//1. Convert array of strings to objects -> text, completed (boolean)
+//2.create a function to remove a todo by text value (case insensitive)
+
+let deleteTodo = function (list, stringToMatch) {
+
+   const indexItemToRemove = list.findIndex(function (item, index) {
+        return item.title.toLowerCase() === stringToMatch.toLowerCase()
+    }
+    )
+    if (indexItemToRemove > -1)
+    {
+        console.log(`deleting ${list[indexItemToRemove].title} which is in position ${indexItemToRemove}`)
+        list.splice(indexItemToRemove, 1)
+        console.log(list)
+    }
+    else{
+        console.log('Item is nowhere to be found')
+    }
+}
+console.log('Now let us remove the eat a healthy breakfast item')
+deleteTodo(todos, 'dont eat a healthy breakfast')
+//console.log(todos)
