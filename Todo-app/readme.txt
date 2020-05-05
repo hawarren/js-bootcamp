d595198dcde74bc0ca853189246b63105a33ff7a
Hanif Warren <Hanif.warren@gmail.com>
2020-05-05 16:00:32 -0400

    challenge 51 adding index page to todoapp

---
+++ Todo-app/readme.txt
@@ -0,0 +1,4 @@
+challenge video 51
+1.Create new html file
+2.add 1 h1 and 5 p tags
+3.Serve that folder and view the doc in the browser.
---
+++ notes-app/notes-app.js
@@ -0,0 +1,12 @@
+//DOM - Document Object Model (a document that is modelled like an object, with methods and properties)
+//the object is called "document"
+console.log('This is from my html index from a different file')
+const p = document.querySelector('p') //gets first element with matching tag
+const ps = document.querySelectorAll('p')
+console.log(p)
+ps.forEach(r => {
+    r.textContent = '*****'
+    //console.log(p.textContent)
+    //p.remove();
+});
+//p.remove();
