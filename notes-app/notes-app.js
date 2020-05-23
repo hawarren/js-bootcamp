5ad136e8eaf0b6d1a596036df9bd5b049c791ca4
Hanif Warren <Hanif.warren@gmail.com>
2020-05-23 14:50:25 -0400

    68 Setting up a third party library

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,56 +1,56 @@
 //DOM - Document Object Model (a document that is modelled like an object, with methods and properties)
 //the object is called "document"
-const notes = [
-    {
+const notes = [{
         title: 'My next trip',
         body: 'I would like to go to spain'
     },
     {
-        title: 'Habits to work on'
-        , body: 'Exercise. Eating a bit better'
+        title: 'Habits to work on',
+        body: 'Exercise. Eating a bit better'
     },
     {
         title: 'Office modification',
         body: 'Get a new seat'
-    }]
+    }
+]
 
 
 const filters = {
-    searchText:''
-}
-// const renderNotes = function (notes, filters){
-//     const filteredNotes = notes.filter(function(note){
-//         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-//     })
-//     document.querySelector('#notes').innerHTML = '' //clear the notes first
-//     //create elements for just the filtered notes
-//     filteredNotes.forEach(function(note){
-//         const noteEl = document.createElement('p')
-//         noteEl.textContent = note.title
-//         document.querySelector('#notes').appendChild(noteEl)
-//     })
-// }
+        searchText: ''
+    }
+    // const renderNotes = function (notes, filters){
+    //     const filteredNotes = notes.filter(function(note){
+    //         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    //     })
+    //     document.querySelector('#notes').innerHTML = '' //clear the notes first
+    //     //create elements for just the filtered notes
+    //     filteredNotes.forEach(function(note){
+    //         const noteEl = document.createElement('p')
+    //         noteEl.textContent = note.title
+    //         document.querySelector('#notes').appendChild(noteEl)
+    //     })
+    // }
 renderNotes(notes, filters)
 
-document.querySelector('#create-note').addEventListener('click',function(event){
+document.querySelector('#create-note').addEventListener('click', function(event) {
     event.target.textContent = 'This button was clicked'
 })
 
-document.querySelector('#search-text').addEventListener('input', function (e){
+document.querySelector('#search-text').addEventListener('input', function(e) {
     console.log(e.target.value)
-    filters.searchText = e.target.value  
+    filters.searchText = e.target.value
     renderNotes(notes, filters)
 })
-document.querySelector('#name-form').addEventListener('submit', function(e){
-    e.preventDefault()//keeps form from being submitted to server (ie the default behavior of submit)
+document.querySelector('#name-form').addEventListener('submit', function(e) {
+    e.preventDefault() //keeps form from being submitted to server (ie the default behavior of submit)
     console.log(e.target.elements.inputTitle.value) //
     console.log(e.target.elements.inputBody.value) //
-    notes.push({title: e.target.elements.inputTitle, body: e.target.elements.inputBody.value})
+    addNotes(e.target.elements.inputTitle.value, e.target.elements.inputBody.value)
     e.target.elements.inputTitle.value = ''
     e.target.elements.inputBody.value = ''
     renderNotes(notes, filters)
 })
 
-document.querySelector('#sortStyle').addEventListener('change', function(e){
+document.querySelector('#sortStyle').addEventListener('change', function(e) {
     console.log(e.target.value) //print the selected item in dropdown
 })
--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -1,39 +1,47 @@
 //create the following functions
 //get saved notes
-let getSavedTodos = function(){
-    return notes
-}
-//Save notes 
-let saveNotes = function (newNote)
-{
-notes.add(newNote)
-}
+let getSavedNotes = function() {
+    return notes;
+};
+let addNotes = function(newTitle, newBody) {
+    //assign a uuid so we can identify that note later
+    notes.push({
+        uuid: uuidv4(),
+        title: newTitle,
+        body: newBody
+    });
+};
+//Save notes
+let saveNotes = function(newNote) {
+    notes.add(newNote);
+};
 //render notes based on filters
-let renderNotes = function(notes, filters)
-{
-    const filteredNotes = notes.filter(function(note){
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-        || note.body.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-    document.querySelector('#notes').innerHTML = '' //clear the notes first
+let renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function(item) {
+        return (
+            item.title.toLowerCase().includes(filters.searchText.toLowerCase()) ||
+            item.body.toLowerCase().includes(filters.searchText.toLowerCase())
+        );
+    });
+    document.querySelector("#notes").innerHTML = ""; //clear the notes first
     //create elements for just the filtered notes
-    
-    generateSummaryDom(filteredNotes.length)
-    generateNoteDom(filteredNotes)
-debugger
-}
+
+    generateSummaryDom(filteredNotes.length);
+    generateNoteDom(filteredNotes);
+    debugger;
+};
 
 //generateNoteDOM
-let generateNoteDom = function(notes){
-    notes.forEach(function(note){
-        const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
-        document.querySelector('#notes').appendChild(noteEl)
-    })
-}
+let generateNoteDom = function(notes) {
+    notes.forEach(function(note) {
+        const noteEl = document.createElement("p");
+        noteEl.textContent = note.title;
+        document.querySelector("#notes").appendChild(noteEl);
+    });
+};
 //generateSummaryDom
-let generateSummaryDom = function(count){
-    const noteHead = document.createElement('h2')
-    noteHead.textContent = `You have ${count} notes`
-        document.querySelector('#notes').appendChild(noteHead)
-}
+let generateSummaryDom = function(count) {
+    const noteHead = document.createElement("h2");
+    noteHead.textContent = `You have ${count} notes`;
+    document.querySelector("#notes").appendChild(noteHead);
+};
