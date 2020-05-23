9e0457e65fb70eb011e9bfc144385e6f51c08f9d
Hanif Warren <Hanif.warren@gmail.com>
2020-05-23 15:00:32 -0400

    68. completed todo challenge (using uuidv4 library to add unique IDs)

--- Todo-app/todo-app.js
+++ Todo-app/todo-app.js
@@ -1,64 +1,25 @@
 let todos = getSavedTodos()
 
-//load up localStorage
-// let todosJson = localStorage.getItem('todos')
-// if (todosJson != null)
-// {
-//     todos = JSON.parse(todosJson)
-// }
 
-// const summary = document.createElement('h2')
-const textFilters ={
-searchText: '',
-hideCompleted: false
-} 
-// const itemsLeftToDo = function()
-// {
-//    return todos.filter(function (item) {
-//         return !item.isDone //returns false when isDone is true (ie item is done so we don't want it in our set)
-//     })
-// } 
+const textFilters = {
+    searchText: '',
+    hideCompleted: false
+}
 
-//this functions renders the todo items not done, and applies filter
-//from the input field on the page.
-// const renderTodos = function (todos, textFilter){
-//     //clear page of todos before rerendering
-//     document.querySelector('#myTodos').textContent = ''
-// const filteredItems = todos.filter(function (item){
-//     return item.title.toLowerCase().includes(textFilters.searchText.toLowerCase())
-    
-// })
-// summary.textContent = `You have ${filteredItems.length} things to do`
-// if (textFilters.hideCompleted)
-// summary.textContent+= '(Filtering completed items)'
-// document.querySelector('#myTodos').appendChild(summary)
-
-//     filteredItems.forEach(function (item) {
-//         let itemToAdd = document.createElement('p')
-//         itemToAdd.textContent = item.title
-//         document.querySelector('#myTodos').appendChild(itemToAdd)
-//     })
-// }
 renderTodos(todos, textFilters)
-document.querySelector('#searchText').addEventListener('input', function (event) {
+document.querySelector('#searchText').addEventListener('input', function(event) {
     textFilters.searchText = event.target.value
-    renderTodos(todos,textFilters)
+    renderTodos(todos, textFilters)
 })
 
-document.querySelector('#todo-form').addEventListener('submit', function (e){
+document.querySelector('#todo-form').addEventListener('submit', function(e) {
     e.preventDefault()
     saveTodos(e.target.elements.todoText.value);
-    // todos.push({title: e.target.elements.todoText.value, isDone: false}) //add todo to our object
-    // console.log(`${e.target.elements.todoText.value} has been added to my todos`)
-    // todosJson = JSON.stringify(todos) //convert to JSON
-    // localStorage.setItem('todos', todosJson) //save to localStorage
-    
     e.target.elements.todoText.value = '';
-    textFilters.searchText = '';    
+    textFilters.searchText = '';
     renderTodos(todos, textFilters)
 })
-document.querySelector('#completedToggle').addEventListener('change', function(e){
+document.querySelector('#completedToggle').addEventListener('change', function(e) {
     textFilters.hideCompleted = e.target.checked // toggle value according to checkbox
     renderTodos(todos, textFilters)
-// textFilters.hideCompleted ? renderTodos(itemsLeftToDo(), textFilters) : renderTodos(todos, textFilters)
 })
--- Todo-app/todo-functions.js
+++ Todo-app/todo-functions.js
@@ -1,37 +1,38 @@
 //challenge: create the following functions
 //fetch todos from local storage
-let getSavedTodos = function () {
-  let todosJson = localStorage.getItem("todos");
-  if (todosJson != null) {
-    return JSON.parse(todosJson);
-  } else {
-    return [];
-  }
+let getSavedTodos = function() {
+    let todosJson = localStorage.getItem("todos");
+    if (todosJson != null) {
+        return JSON.parse(todosJson);
+    } else {
+        return [];
+    }
 };
 
 //Save todos to local storage
-const saveTodos = function (title) {
-  todos.push({ title: title, isDone: false });
-  todosJson = JSON.stringify(todos);
-  localStorage.setItem("todos", todosJson);
-  console.log(`${title} has been added to the todo list`);
+const saveTodos = function(title) {
+    //add a uuid to our title
+    todos.push({ uuid: uuidv4(), title: title, isDone: false });
+    todosJson = JSON.stringify(todos);
+    localStorage.setItem("todos", todosJson);
+    console.log(`${title} has been added to the todo list`);
 };
 
 //Render application todos based on filters
-let renderTodos = function (todos, textFilter) {
-  document.querySelector("#myTodos").textContent = "";
-  let filteredItems = todos.filter(function (item) {
-    let hasText = item.title.toLowerCase().includes(textFilters.searchText);
-    //include if it has the text value we filtered on
-    return hasText;
-  });
-  if (textFilters.hideCompleted) {
-    //filter out completed items in place (filter and assign back to same variable)
-    filteredItems = filteredItems.filter(function (item) {
-      return !item.isDone;
+let renderTodos = function(todos, textFilter) {
+    document.querySelector("#myTodos").textContent = "";
+    let filteredItems = todos.filter(function(item) {
+        let hasText = item.title.toLowerCase().includes(textFilters.searchText);
+        //include if it has the text value we filtered on
+        return hasText;
     });
-  }
-  generateTodoDOM(filteredItems);
+    if (textFilters.hideCompleted) {
+        //filter out completed items in place (filter and assign back to same variable)
+        filteredItems = filteredItems.filter(function(item) {
+            return !item.isDone;
+        });
+    }
+    generateTodoDOM(filteredItems);
 };
 
 //1. Set up a root div
@@ -40,33 +41,33 @@ let renderTodos = function (todos, textFilter) {
 //4. Setup and append a button (set text)
 
 //Get the DOM elements for an individual note
-let generateTodoDOM = function (filteredList) {
-  if (filteredList == undefined) {
-    console.log("generateTodoDOM: No filteredlist to print");
-  } else {
-    generateSummaryDOM(filteredList.length);
-    filteredList.forEach(function (item) {
-      //Setup the pieces to create a single line checkbox with the text title
-      const rootDiv = document.createElement("div"); //element to hold our todo with checkbox in it on one line
-      const mySpan = document.createElement("span");
-      mySpan.textContent = item.title;
-      const thisCheckBox = document.createElement("input");
-      thisCheckBox.setAttribute('type','checkbox'); //create a checkbox and set it's type
-      const removeButtonEl = document.createElement("button");
-      removeButtonEl.textContent = "x";
-      //add our stuff to the rootdiv and then append it to the DOM
-      rootDiv.appendChild(thisCheckBox);
-      rootDiv.appendChild(mySpan);
-      rootDiv.appendChild(removeButtonEl);     
+let generateTodoDOM = function(filteredList) {
+    if (filteredList == undefined) {
+        console.log("generateTodoDOM: No filteredlist to print");
+    } else {
+        generateSummaryDOM(filteredList.length);
+        filteredList.forEach(function(item) {
+            //Setup the pieces to create a single line checkbox with the text title
+            const rootDiv = document.createElement("div"); //element to hold our todo with checkbox in it on one line
+            const mySpan = document.createElement("span");
+            mySpan.textContent = item.title;
+            const thisCheckBox = document.createElement("input");
+            thisCheckBox.setAttribute('type', 'checkbox'); //create a checkbox and set it's type
+            const removeButtonEl = document.createElement("button");
+            removeButtonEl.textContent = "x";
+            //add our stuff to the rootdiv and then append it to the DOM
+            rootDiv.appendChild(thisCheckBox);
+            rootDiv.appendChild(mySpan);
+            rootDiv.appendChild(removeButtonEl);
 
-      document.querySelector("#myTodos").appendChild(rootDiv);
-    });
-  }
+            document.querySelector("#myTodos").appendChild(rootDiv);
+        });
+    }
 };
 
 //Get the Dom elements for the list summary
-let generateSummaryDOM = function (count) {
-  let summaryEl = document.createElement("h2");
-  summaryEl.textContent = `You have ${count} items left to do`;
-  document.querySelector("#myTodos").appendChild(summaryEl);
+let generateSummaryDOM = function(count) {
+    let summaryEl = document.createElement("h2");
+    summaryEl.textContent = `You have ${count} items left to do`;
+    document.querySelector("#myTodos").appendChild(summaryEl);
 };
