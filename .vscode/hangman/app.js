2ef56009b1527436eb35c931810e7a84dda5d355
Hanif Warren <Hanif.warren@gmail.com>
2020-08-31 20:40:14 -0400

    moved hangman to it's own folder

---
+++ .vscode/hangman/app.js
@@ -0,0 +1,25 @@
+//primitive value: there are 5 primitive values: string, number, boolean, null, undefined
+// const product = {
+//     name: 'Influence'
+// }
+
+// Object.prototype.someNewMethod = () => 'This is the new function'
+// console.log(product.someNewMethod())
+// console.log(product)
+
+//prototype chain
+//Object: myArray --> Array.protoype --> Object.prototype --> null
+const team = ['Luke', 'Madison']
+console.log(team)
+
+//function: myFunc --> function.prototype --> Object.prototype -->
+const getScore = () => 1
+
+console.log(getScore)
+    //String: myString --> String.Prototype - --> Object.prototype --> null
+const product = "computer"
+console.log(product)
+
+//Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+const otherProduct = new String('Phone')
+console.log(otherProduct)
