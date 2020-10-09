503da72e277bbb9fda2ee1e1759697c6d88b7df2
Hanif Warren <Hanif.warren@gmail.com>
2020-10-09 03:01:45 -0400

    Exploring another api

--- hangman/app.js
+++ hangman/app.js
@@ -1,8 +1,6 @@
 //HTTP (HypterText Transfer Protocol)
 //Request = What do we want to do
-//Response- The answer we get back
-
-
+//Response- what was actually done
 
 const exampleFour = new HangManGame("Lipid is a fat", 5);
 
@@ -18,7 +16,7 @@ window.addEventListener("keypress", function(e) {
     renderPuzzle(exampleFour.puzzle);
 });
 
-const renderPuzzle = function(myPuzzle) {
+const renderPuzzle = (myPuzzle) => {
     exampleFour.calculateStatus();
     const vDom = document.getElementById('hResult');
     const pResultDom = document.createElement('div');
@@ -37,12 +35,39 @@ renderPuzzle(exampleFour.puzzle);
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        console.log(e.target.status)
         const data = JSON.parse(e.target.responseText)
         console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place')
     }
 
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
+
+
+
+//1. make a call to restcountries.eu/rest/v2
+//1. Make a new request for all countries
+//2.Parse the responseText to get back the array of objects
+//3. Find your country object by it's country code (alpha2Code property)
+//4.Print the full country name
+//parse the json that comes back
+const countryRequest = new XMLHttpRequest()
+countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
+countryRequest.send()
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        console.log(e.target.status)
+        const data = JSON.parse(e.target.responseText)
+        const myCountry = data.filter(item => {
+            return item.alpha2Code == "US"
+        })
+        console.log(`my country name is ${myCountry[0]['name']}`)
+
+    }
+})
