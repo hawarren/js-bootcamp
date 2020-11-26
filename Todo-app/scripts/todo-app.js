e9276dded5a7369dc4b879b8e3c55a74373031bf
Hanif Warren <Hanif.warren@gmail.com>
2020-11-25 22:03:52 -0500

    126 up to 1m32, added filter for empty input

--- Todo-app/scripts/todo-app.js
+++ Todo-app/scripts/todo-app.js
@@ -13,8 +13,15 @@ document.querySelector('#searchText').addEventListener('input', (event) => {
     renderTodos(todos, textFilters)
 })
 
+
+//1. get trimmed version of the input
+//2. only add new todo if trimmed version has content
 document.querySelector('#todo-form').addEventListener('submit', (e) => {
     e.preventDefault()
+    if (e.target.elements.todoText.value.trim() == '') {
+        console.log(` the input ${e.target.elements.todoText.value} has no content`)
+        return
+    }
     todos.push({ uuid: uuidv4(), title: e.target.elements.todoText.value, isDone: false }); //add a uuid to our title
     saveTodos();
     console.log(`${e.target.elements.todoText.value} has been added to the todo list`);
