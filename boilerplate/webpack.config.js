ceff0ad949d01f3628fdbd4e9ba0407d769d5795
Hanifw <hanif.warren@gmail.com>
2020-12-18 19:49:28 -0500

    137 adding babel into webpack with module rule (using babel loader)

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -5,7 +5,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'public/scripts'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
     }
+
 }
 
 
