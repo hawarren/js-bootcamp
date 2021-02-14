dc1bd2fce73aeaf732d7a77c01b634d73f9e831d
Hanif Warren <hanif.warren@gmail.com>
2021-02-13 20:10:05 -0500

    151 The object spread syntax. installed babel-plugin-transform-object-rest-spread to use the rest and spread operators. Wrote prototype code to use rest and spread on objects

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -38,3 +38,38 @@ cities = ['Los Vegas',...cities]
 
 console.log(`First array is ${cities}`)
 //console.log(`Second array is ${citiesCopy}`)
+ let house = {
+    bedrooms: 2,
+    bathrooms: 1.5,
+    year: 1027
+
+ }
+ let newHouse = {
+     basement: true,
+            ...house,
+            bedrooms: 5
+ }
+ newHouse.year = 2018
+ console.log(house)
+ console.log(newHouse)
+
+ // Create a person object with name and age
+ // Create a location object with city and country
+ // Creat a new overview object and use the spread operator to add all four properties
+
+ let person = {
+     name: 'Hanif',
+     age: 39
+
+ }
+ let location = {
+     city: 'Piscataway',
+     country: 'USA'
+ }
+ let overView = {
+     ...person,
+     ...location
+ }
+ console.log(person)
+ console.log(location)
+ console.log(overView)
